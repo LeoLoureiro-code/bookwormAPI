ea92daf4a089469c150b42596d2e3282c74c0384
Leo <leolou.90@gmail.com>
2025-07-17 18:38:47 -0700

    Create interfaces folder and interface for user repository

---
+++ bookwormAPI.EF.DataAccess/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using bookwormAPI.EF.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookwormAPI.EF.DataAccess.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+
+        Task<User> GetUserById(int id);
+
+        Task<User> Createuser(User user);
+
+        Task<User> Updateuser(User user);
+
+        Task<User> Deleteuser(int id);
+
+    }
+}
