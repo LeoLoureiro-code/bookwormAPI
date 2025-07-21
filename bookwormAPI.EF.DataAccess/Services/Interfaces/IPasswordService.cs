73699d2beeaab5ae1be77db16d8979ca507da8ee
Leo <leolou.90@gmail.com>
2025-07-20 18:54:12 -0700

    Create password hasher services and interface

---
+++ bookwormAPI.EF.DataAccess/Services/Interfaces/IPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookwormAPI.EF.DataAccess.Services.Interfaces
+{
+    public interface IPasswordService
+    {
+        string HashPassword(string password);
+
+        bool VerifyPassword(string hashedPassword, string providedPassword);
+    }
+}
---
+++ bookwormAPI.EF.DataAccess/Services/PasswordService.cs
@@ -0,0 +1,27 @@
+﻿using bookwormAPI.EF.DataAccess.Services.Interfaces;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookwormAPI.EF.DataAccess.Services
+{
+    public class PasswordService: IPasswordService
+    {
+        private readonly PasswordHasher<string> _hasher = new PasswordHasher<string>();
+
+        public string HashPassword(string password)
+        {
+            return _hasher.HashPassword(null, password);
+        }
+
+        public bool VerifyPassword(string hashedPassword, string providedPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+            return result == Microsoft.AspNetCore.Identity.PasswordVerificationResult.Success;
+        }
+    }
+}
