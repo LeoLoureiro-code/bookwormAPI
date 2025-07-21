cd804385c6f1e292ebf980aba01bd728d6d71879
Leo <leolou.90@gmail.com>
2025-07-20 18:52:53 -0700

    Registered dependencies

--- bookwormAPI/Program.cs
+++ bookwormAPI/Program.cs
@@ -1,3 +1,9 @@
+using bookwormAPI.EF.DataAccess.Context;
+using bookwormAPI.EF.DataAccess.Repositories;
+using bookwormAPI.EF.DataAccess.Repositories.Interfaces;
+using bookwormAPI.EF.DataAccess.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<PasswordService>();
+
+// Configura el DbContext usando la cadena de conexión de appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Registra el DbContext para inyección de dependencias
+builder.Services.AddDbContext<BookwormContext>(options =>
+{
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
+    options.UseMySql(connectionString, serverVersion);
+});
 
 var app = builder.Build();
 
