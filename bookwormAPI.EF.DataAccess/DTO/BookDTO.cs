319d2da4df9aee617a66f3c0479858c4c3736bc0
Leo <leolou.90@gmail.com>
2025-07-21 17:38:45 -0700

    Create book DTO

---
+++ bookwormAPI.EF.DataAccess/DTO/BookDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookwormAPI.EF.DataAccess.DTO
+{
+    public class BookDTO
+    {
+        public string BookTitle { get; set; } = null!;
+
+        public string BookAuthor { get; set; } = null!;
+
+        public int BookPages { get; set; }
+
+        public string BookStatus { get; set; } = null!;
+
+        public string? BookFeeling { get; set; }
+    }
+}
