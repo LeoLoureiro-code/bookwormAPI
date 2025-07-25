6ec78b7a49636d5d133e85a61cade9cf08db96f5
Leo <leolou.90@gmail.com>
2025-07-24 18:05:07 -0700

    Add auth service

---
+++ bookwormAPI.EF.DataAccess/Services/AuthService.cs
@@ -0,0 +1,15 @@
+﻿using bookwormAPI.EF.DataAccess.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookwormAPI.EF.DataAccess.Services
+{
+    public class AuthService: IAuthService
+    {
+
+
+    }
+}
