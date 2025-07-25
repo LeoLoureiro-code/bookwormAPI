bbb4e9cc7dbdad01f050fcba835a3f65a8d2bddf
Leo <leolou.90@gmail.com>
2025-07-24 18:04:52 -0700

    Add authorization service

---
+++ bookwormAPI.EF.DataAccess/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookwormAPI.EF.DataAccess.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> GetAuth(string username, string password);
+
+    }
+}
