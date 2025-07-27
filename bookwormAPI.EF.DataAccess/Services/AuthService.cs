5375c47b4554e6ae8d0e47a03a98d26c2622561a
Leo <leolou.90@gmail.com>
2025-07-27 01:17:41 -0700

    Implement method login to auth interface

--- bookwormAPI.EF.DataAccess/Services/AuthService.cs
+++ bookwormAPI.EF.DataAccess/Services/AuthService.cs
@@ -1,7 +1,12 @@
-﻿using bookwormAPI.EF.DataAccess.Services.Interfaces;
+﻿using bookwormAPI.EF.DataAccess.Repositories.Interfaces;
+using bookwormAPI.EF.DataAccess.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +15,50 @@ namespace bookwormAPI.EF.DataAccess.Services
     public class AuthService: IAuthService
     {
 
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordService _passwordService;
+        private readonly IConfiguration _config;
 
+        public AuthService(IUserRepository userRepository, IPasswordService passwordService, IConfiguration config)
+        {
+            _userRepository = userRepository;
+            _passwordService = passwordService;
+            _config = config;
+        }
+
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            //Look user
+            var user = await _userRepository.GetUserByNameAndPasswordAsync(username, password);
+            if (user == null)
+                throw new Exception("User not found.");
+
+            //Verify password
+            bool isValid = _passwordService.VerifyPassword(user.UserPasswordHash, password);
+            if (!isValid)
+                throw new Exception("Invalid password.");
+
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_config["Jwt:Key"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                Issuer = _config["Jwt:Issuer"],
+                Audience = _config["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
