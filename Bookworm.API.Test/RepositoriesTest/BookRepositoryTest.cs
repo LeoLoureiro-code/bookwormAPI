cdbbe9d111b4617c1e7ed59792371b47119fc03d
Leo <leolou.90@gmail.com>
2025-08-10 17:13:39 -0700

    Add tests to repositories

---
+++ Bookworm.API.Test/RepositoriesTest/BookRepositoryTest.cs
@@ -0,0 +1,154 @@
+﻿using bookwormAPI.EF.DataAccess.Context;
+using bookwormAPI.EF.DataAccess.Models;
+using bookwormAPI.EF.DataAccess.Repositories;
+using bookwormAPI.EF.DataAccess.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+
+public class BookRepositoryTests
+{
+    private BookwormContext GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<BookwormContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()) 
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        return new BookwormContext(options);
+    }
+
+    [Fact]
+    public async Task CreateBook_ShouldAddBook()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+
+        var bookDto = new BookDTO
+        {
+            Title = "Test Book",
+            Author = "Author Name",
+            Pages = 100,
+            Status = "Read",
+            Feeling = "Excited",
+        };
+
+        var createdBook = await repo.CreateBook(bookDto);
+
+        Assert.NotNull(createdBook);
+        Assert.Equal("Test Book", createdBook.BookTitle);
+        Assert.Equal("Author Name", createdBook.BookAuthor);
+        Assert.Equal("Read", createdBook.BookStatus);
+        Assert.Equal(100, createdBook.BookPages);
+        Assert.Equal("Excited", createdBook.BookFeeling);
+    }
+
+    [Fact]
+    public async Task GetBookById_ShouldReturnBook_WhenExists()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+
+        var book = new Book
+        {
+            BookTitle = "Sample",
+            BookAuthor = "Author",
+            BookPages = 123,
+            BookStatus = "to buy",
+            BookFeeling = "Happy"
+        };
+        context.Books.Add(book);
+        await context.SaveChangesAsync();
+
+        var result = await repo.GetBookById(book.BookId);
+
+        Assert.NotNull(result);
+        Assert.Equal("Sample", result.BookTitle);
+    }
+
+    [Fact]
+    public async Task GetBookById_ShouldThrowException_WhenNotFound()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+
+        await Assert.ThrowsAsync<Exception>(() => repo.GetBookById(999));
+    }
+
+   
+    [Fact]
+    public async Task UpdateBook_ShouldModifyBook()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+
+        var book = new Book
+        {
+            BookTitle = "Old Title",
+            BookAuthor = "Old Author",
+            BookPages = 200,
+            BookFeeling = "Neutral",
+            BookStatus = "Unread"
+        };
+        context.Books.Add(book);
+        await context.SaveChangesAsync();
+
+        var updatedBook = await repo.UpdateBook(
+            book.BookId,
+            "New Title",
+            "New Author",
+            300,
+            "Read",
+            "Excited");
+
+        Assert.Equal("New Title", updatedBook.BookTitle);
+        Assert.Equal("New Author", updatedBook.BookAuthor);
+        Assert.Equal(300, updatedBook.BookPages);
+        Assert.Equal("Read", updatedBook.BookStatus);
+        Assert.Equal("Excited", updatedBook.BookFeeling);
+    }
+
+    [Fact]
+    public async Task UpdateBook_ShouldThrowException_WhenBookNotFound()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+
+        await Assert.ThrowsAsync<Exception>(() =>
+            repo.UpdateBook(999, "Title", "Author", 100, "Status", "Feeling"));
+    }
+
+    [Fact]
+    public async Task DeleteBook_ShouldRemoveBook()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+
+        var book = new Book
+        {
+            BookTitle = "To be deleted",
+            BookAuthor = "Author",
+            BookPages = 150,
+            BookStatus = "read",
+            BookFeeling = "Sad"
+        };
+        context.Books.Add(book);
+        await context.SaveChangesAsync();
+
+        await repo.DeleteBook(book.BookId);
+
+        var deletedBook = await context.Books.FindAsync(book.BookId);
+        Assert.Null(deletedBook);
+    }
+
+    [Fact]
+    public async Task DeleteBook_ShouldThrowException_WhenBookNotFound()
+    {
+        using var context = GetDbContext();
+        var repo = new BookRepository(context);
+        await Assert.ThrowsAsync<Exception>(() => repo.DeleteBook(999));
+    }
+}
---
+++ Bookworm.API.Test/RepositoriesTest/UserReposirotyTest.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using bookwormAPI.DTO;
+using bookwormAPI.EF.DataAccess.Context;
+using bookwormAPI.EF.DataAccess.Models;
+using bookwormAPI.EF.DataAccess.Repositories;
+using bookwormAPI.EF.DataAccess.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+public class UserRepositoryTests
+{
+    private BookwormContext GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<BookwormContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new BookwormContext(options);
+    }
+
+    private Mock<IPasswordService> GetPasswordServiceMock()
+    {
+        var mock = new Mock<IPasswordService>();
+        mock.Setup(p => p.HashPassword(It.IsAny<string>())).Returns("hashed_password");
+        return mock;
+    }
+
+    [Fact]
+    public async Task CreateUser_ShouldAddUser_WhenEmailIsUnique()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+
+        var repo = new UserRepository(context, passwordService.Object);
+
+        var userDto = new UserDTO { Email = "test@example.com", Password = "123456" };
+
+        var createdUser = await repo.CreateUser(userDto);
+
+        Assert.NotNull(createdUser);
+        Assert.Equal("test@example.com", createdUser.UserName);
+        Assert.Equal("hashed_password", createdUser.UserPasswordHash);
+        Assert.Single(context.Users);
+    }
+
+    [Fact]
+    public async Task CreateUser_ShouldThrow_WhenEmailExists()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        context.Users.Add(new User { UserName = "test@example.com", UserPasswordHash = "hash" });
+        await context.SaveChangesAsync();
+
+        var userDto = new UserDTO { Email = "test@example.com", Password = "123456" };
+
+        await Assert.ThrowsAsync<Exception>(() => repo.CreateUser(userDto));
+    }
+
+    [Fact]
+    public async Task GetUserById_ShouldReturnUser_WhenExists()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        var user = new User { UserName = "test@example.com", UserPasswordHash = "hash" };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        var result = await repo.GetUserById(user.UserId);
+
+        Assert.NotNull(result);
+        Assert.Equal(user.UserName, result.UserName);
+    }
+
+    [Fact]
+    public async Task GetUserById_ShouldThrow_WhenNotFound()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        await Assert.ThrowsAsync<Exception>(() => repo.GetUserById(999));
+    }
+
+    [Fact]
+    public async Task GetUserByName_ShouldReturnUser_WhenExists()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        var user = new User { UserName = "test@example.com", UserPasswordHash = "hash" };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        var result = await repo.GetUserByName("test@example.com");
+
+        Assert.NotNull(result);
+        Assert.Equal(user.UserName, result.UserName);
+    }
+
+    [Fact]
+    public async Task GetUserByName_ShouldThrow_WhenNotFound()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        await Assert.ThrowsAsync<Exception>(() => repo.GetUserByName("notfound@example.com"));
+    }
+
+    [Fact]
+    public async Task UpdateUser_ShouldUpdateData_WhenExists()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        var user = new User { UserName = "old@example.com", UserPasswordHash = "oldhash" };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        var updated = await repo.UpdateUser(user.UserId, "new@example.com", "newhash");
+
+        Assert.Equal("new@example.com", updated.UserName);
+        Assert.Equal("newhash", updated.UserPasswordHash);
+    }
+
+    [Fact]
+    public async Task UpdateUser_ShouldThrow_WhenNotFound()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        await Assert.ThrowsAsync<Exception>(() => repo.UpdateUser(999, "name", "hash"));
+    }
+
+    [Fact]
+    public async Task DeleteUser_ShouldRemove_WhenExists()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        var user = new User { UserName = "test@example.com", UserPasswordHash = "hash" };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        await repo.DeleteUser(user.UserId);
+
+        Assert.Empty(context.Users);
+    }
+
+    [Fact]
+    public async Task DeleteUser_ShouldThrow_WhenNotFound()
+    {
+        var context = GetDbContext();
+        var passwordService = GetPasswordServiceMock();
+        var repo = new UserRepository(context, passwordService.Object);
+
+        await Assert.ThrowsAsync<Exception>(() => repo.DeleteUser(999));
+    }
+}
